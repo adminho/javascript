6a7d524dcec535bc9cb3fcf9802389b2e5d5bb6c
adminho <couragor@gmail.com>
2023-09-22 19:24:53 +0700

    no message

--- examples_book/init_page.js
+++ examples_book/init_page.js
@@ -1,23 +1,28 @@
 	let targetDiv = document.getElementById("main");
 	let statusLoading = document.getElementById("statusLoading");	
 	let bottomAds = document.getElementById("bottom-ads");
-	let healineDiv = document.getElementById("headline")
-	bottomAds.style.display = "none";				
+	let healineDiv = document.getElementById("headline");
+	let mainMenu = document.getElementById("main-menu");
+		
+	function isDesktop(){
+		//if ( WURFL.form_factor === "Desktop") {		     
+		if(  window.innerWidth >=768 ) {		
+			return true;
+				
+		} else {
+				//let str =link.href.split("chapter")[1];
+				//let no = str.replace(".html", "");						
+				//link.href = `chapter.php?no=${parseInt(no)-1}`;	
+			return false;	
+		}	
+	}
 
 	let allLink = document.getElementsByClassName("link-chap");
 	for(const link of allLink) {
-		link.addEventListener('click', function(event) {					
-			//if ( WURFL.form_factor === "Desktop") {		     
-			if(  window.innerWidth >=768 ) {					
-				event.preventDefault();	
-				let file = link.href;		
-				includeHTML(link);
-			} else {
-				let str =link.href.split("chapter")[1];
-				let no = str.replace(".html", "");						
-				link.href = `chapter.php?no=${parseInt(no)-1}`;							
-			}			
-			
+		link.addEventListener('click', function(event) {	
+			event.preventDefault();	
+			let file = link.href;		
+			includeHTML(link);		
 		});
 		
 		link.addEventListener('contextmenu', function(event) {
@@ -25,8 +30,11 @@
 		});
 	}			
 			
-	function includeHTML(link) {	
-	    
+	function showMenu() {
+		mainMenu.style.display = "block";			
+	}
+	
+	function includeHTML(link) {		    
 		if(!link){
 			throw new Error(`Not have a link`);
 		}
@@ -52,6 +60,11 @@
 				targetDiv.innerHTML = text;		
 				bottomAds.style.display = "block";	
 			}
+						
+			if(  !isDesktop()) {					
+				mainMenu.style.display = "none";	
+			}
+			
 		 }
 		)
 		.catch( err => {
@@ -61,7 +74,9 @@
 		});
     }
  
-   /*
-	function selectLink(index) {		
-		document.getElementsByClassName("link-chap")[index].click();
-	}*/
+   	includeHTML(document.getElementsByClassName("link-chap")[1]); // select default link
+	
+	window.resize = function(){		
+		alert();
+		!isDesktop?mainMenu.style.display = "block":mainMenu.style.display = "none";
+	}
